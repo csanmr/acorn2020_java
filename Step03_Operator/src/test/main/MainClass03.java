de0fc30c7ac86be7030e0f30aca88e1648b61048
Taekgeon <ggooggllee012@gmail.com>
2020-05-28 17:37:12 +0900

    Step03_Operator 추가함

--- Step03_Operator/src/test/main/MainClass03.java
+++ Step03_Operator/src/test/main/MainClass03.java
@@ -1,6 +1,8 @@
 package test.main;
 /*
  *  3. 비교연산자 테스트
+ *  
+ *  ==, !=, >, >=, <, <=
  */
 public class MainClass03 {
 	public static void main(String[] args) {
@@ -11,9 +13,10 @@ public class MainClass03 {
 		boolean result4 = 10 >= 10; //true
 		boolean result5 = 10 < 100; //true
 		boolean result6 = 10 <= 10; //true	
-		
+		//String type변수 name에 null대입하기
 		String name=null;
 		
+		//null인지 아닌지 비교가 가능하다(즉 java에서 null값은 비교가 가능(자바스크립트는 불가))
 		boolean result7 = name == null; //true
 		boolean result8 = name != null; //false
 	}
--- Step03_Operator/src/test/main/MainClass06.java
+++ Step03_Operator/src/test/main/MainClass06.java
@@ -5,9 +5,10 @@ package test.main;
 public class MainClass06 {
 	public static void main(String[] args) {
 		boolean isWait=false;
-		String result = isWait ? "기다려요" : "기다리지 않아요";
+		String result = isWait ? "기다려요" : "기다리지 않아요"; // isWait가 true면 기다려요 false면 기다리지 않아요
 		System.out.println(result);
 		
+		//위의 3항 연산자는 아래와 같이 if~ else 문과 같은 로직이다.
 		String result2=null;
 		if(isWait) {
 			result2="기다려요";
